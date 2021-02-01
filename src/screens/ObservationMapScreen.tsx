8d687f8c7036b51a7da1adaca1085b7645942036
Ismael Haddad <ismael.haddad@prototyp.se>
2021-02-01 15:42:09 +0100

    implement map view

--- src/screens/ObservationMapScreen.tsx
+++ src/screens/ObservationMapScreen.tsx
@@ -1,10 +1,60 @@
 import * as React from "react";
 import styled from "../styled";
+import MapView, { Marker } from "react-native-maps";
+import { Dimensions, Image } from "react-native";
+import { useSelector } from "react-redux";
+import { RootState } from "../store/store";
+import { Observation } from "../store/slices/observations";
 
 export default function ObservationMapScreen() {
+  const observationsEntries = useSelector<RootState, Array<Observation>>(
+    (state) => state.observations.entries
+  );
+
   return (
     <Screen>
-      <Text>Here goes a map</Text>
+      <MapView
+        style={{
+          width: Dimensions.get("window").width,
+          height: Dimensions.get("window").height,
+        }}
+      >
+        {observationsEntries
+          .filter((o) => o.location && o.location.latitude)
+          .map((observationEntry, index) => {
+            if (
+              !observationEntry.location ||
+              !observationEntry.location.latitude
+            )
+              return;
+            return (
+              <Marker
+                key={index}
+                coordinate={{
+                  latitude: observationEntry.location.latitude,
+                  longitude: observationEntry.location.longitude,
+                }}
+                title={`Observer: ${observationEntry.observer}`}
+                description={
+                  observationEntry.timestamp
+                    ? new Date(observationEntry.timestamp)
+                        .toUTCString()
+                        .slice(5, 17)
+                    : ""
+                }
+              >
+                <MarkerPopup>
+                  {observationEntry.image ? (
+                    <Image
+                      source={{ uri: observationEntry.image }}
+                      style={{ width: 50, height: 50, borderRadius: 6 }}
+                    />
+                  ) : null}
+                </MarkerPopup>
+              </Marker>
+            );
+          })}
+      </MapView>
     </Screen>
   );
 }
@@ -13,6 +63,11 @@ const Screen = styled.View`
   flex: 1;
   align-items: center;
   justify-content: center;
+`;
+
+const MarkerPopup = styled.View`
+  border-radius: 6px;
+  background-color: white;
 `;
 
 const Text = styled.Text`
