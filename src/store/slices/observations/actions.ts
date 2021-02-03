79b1413af3b8569393b2b8c90b618288efd9ec84
Ismael Haddad <ismael.haddad@prototyp.se>
2021-02-03 14:39:15 +0100

    debugging

--- src/store/slices/observations/actions.ts
+++ src/store/slices/observations/actions.ts
@@ -27,40 +27,40 @@ export const fetchAllObservations: Thunk = () => async (
 export const submitNewObservation: Thunk<NewObservationPayload> = (
   newObservationPayload
 ) => async (dispatch, _, { api, localStorage, navigation }) => {
-  const newFeatures: Array<Feature> = newObservationPayload.features.map(
-    (featurePayload) => ({
-      id: uuidv4(),
-      creatorId: "CREATOR_ID", // Relation with "creator" (model User)
-      creatorApp: CreatorApps.DATA_COLLECTION_APP,
-      createdAt: new Date(Date.now()).toISOString(),
-      updatedAt: new Date(Date.now()).toISOString(),
-      isDeleted: false,
-      deletedAt: undefined,
+  // const newFeatures: Array<Feature> = newObservationPayload.features.map(
+  //   (featurePayload) => ({
+  //     id: uuidv4(),
+  //     creatorId: "CREATOR_ID", // Relation with "creator" (model User)
+  //     creatorApp: CreatorApps.DATA_COLLECTION_APP,
+  //     createdAt: new Date(Date.now()).toISOString(),
+  //     updatedAt: new Date(Date.now()).toISOString(),
+  //     isDeleted: false,
+  //     deletedAt: undefined,
 
-      comments: featurePayload.comments,
-      imageUrl: featurePayload.imageUrl,
-    })
-  );
-  const newObservation: Observation = {
-    id: uuidv4(),
-    creatorId: "CREATOR_ID", // Relation with "creator" (model User)
-    creatorApp: CreatorApps.DATA_COLLECTION_APP,
-    createdAt: new Date(Date.now()).toISOString(),
-    updatedAt: new Date(Date.now()).toISOString(),
-    isDeleted: false,
-    deletedAt: undefined,
+  //     comments: featurePayload.comments,
+  //     imageUrl: featurePayload.imageUrl,
+  //   })
+  // );
+  // const newObservation: Observation = {
+  //   id: uuidv4(),
+  //   creatorId: "CREATOR_ID", // Relation with "creator" (model User)
+  //   creatorApp: CreatorApps.DATA_COLLECTION_APP,
+  //   createdAt: new Date(Date.now()).toISOString(),
+  //   updatedAt: new Date(Date.now()).toISOString(),
+  //   isDeleted: false,
+  //   deletedAt: undefined,
 
-    geometry: newObservationPayload.geometry,
-    timestamp: newObservationPayload.timestamp.toISOString(),
-    comments: newObservationPayload.comments,
-    isMatched: false,
-    features: newFeatures,
-  };
+  //   geometry: newObservationPayload.geometry,
+  //   timestamp: newObservationPayload.timestamp.toISOString(),
+  //   comments: newObservationPayload.comments,
+  //   isMatched: false,
+  //   features: newFeatures,
+  // };
 
-  //const isSuccess: boolean = false; //await api.mockPOSTNewObservation(newObservation);
-  //if (!isSuccess) await localStorage.queueObservation(newObservation);
-  dispatch(addNewObservation(newObservation));
-  dispatch(resetFeaturesToAdd());
+  // const isSuccess: boolean = false; //await api.mockPOSTNewObservation(newObservation);
+  // if (!isSuccess) await localStorage.queueObservation(newObservation);
+  // dispatch(addNewObservation(newObservation));
+  // dispatch(resetFeaturesToAdd());
   navigation.navigate("observationList");
 };
 
