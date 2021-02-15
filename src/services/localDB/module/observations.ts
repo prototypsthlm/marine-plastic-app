2ecfcec03389ce1363c25bd7001e94347a1f10dd
Ismael Haddad <ismael.haddad@prototyp.se>
2021-02-15 13:58:59 +0100

    fetch & cache observations

--- src/services/localDB/module/observations.ts
+++ src/services/localDB/module/observations.ts
@@ -1,31 +1,64 @@
-import { Observation } from "../../../models";
+import { Campaign, Feature, Observation } from "../../../models";
 import { db } from "../db";
 import { EntityType } from "../types";
 
+type EntityPayload = Array<Observation | Feature | Campaign>;
+
 export const observationsModule = {
-  storeOfflineObservation(observationPayload: Observation) {
-    db.transaction((tx) => {
-      tx.executeSql(
-        "insert into baseEntity (id, isSynced, type, jsonObject) values (?, 0, ?, ?)",
-        [
-          observationPayload.id,
-          EntityType.Observation,
-          JSON.stringify(observationPayload),
-        ]
+  upsertEntities(
+    payloadArray: EntityPayload,
+    entityType: EntityType,
+    isSynced: boolean,
+    campaignId: string | null = null,
+    observationId: string | null = null
+  ) {
+    const insertValues = payloadArray.map(
+      (payload) =>
+        `('${payload.id}', ${
+          isSynced ? "1" : "0"
+        }, '${entityType}', '${campaignId}', '${observationId}', '${JSON.stringify(
+          payload
+        )}')`
+    );
+    return new Promise<void>((resolve, reject) => {
+      db.transaction(
+        (tx) => {
+          tx.executeSql(
+            `insert into baseEntity (id, isSynced, type, campaignId, observationId, jsonObject) values ${insertValues.join(
+              ", "
+            )} on conflict(id) do update set isSynced=excluded.isSynced, jsonObject=excluded.jsonObject`
+          );
+        },
+        reject,
+        resolve
       );
     });
   },
-  getAllOfflineObservations() {
-    db.transaction((tx) => {
-      tx.executeSql(
-        "select * from baseEntity where type = ? and isSynced = 0",
-        [EntityType.Observation],
-        (_, { rows }) => {
-          for (let i = 0; i < rows.length; i++) {
-            console.log(rows.item(i));
+  getEntities<T>(
+    entityType: EntityType,
+    campaignId: string | null = null,
+    observationId: string | null = null
+  ): Promise<Array<T>> {
+    return new Promise<Array<T>>((resolve) => {
+      db.transaction((tx) => {
+        const filerByCampaign =
+          campaignId !== null ? `and campaignId = '${campaignId}'` : "";
+        const filerByObservation =
+          observationId !== null
+            ? `and observationId = '${observationId}'`
+            : "";
+        tx.executeSql(
+          `select * from baseEntity where type = ? ${filerByCampaign} ${filerByObservation}`,
+          [entityType, campaignId, observationId],
+          (_, { rows }) => {
+            let entities: Array<T> = [];
+            for (let i = 0; i < rows.length; i++) {
+              entities = [...entities, JSON.parse(rows.item(i).jsonObject)];
+            }
+            resolve(entities);
           }
-        }
-      );
+        );
+      });
     });
   },
 };
