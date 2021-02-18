0a6b2bc38a737137daf62e1a99cd595078116408
Ismael Haddad <ismael.haddad@prototyp.se>
2021-02-18 16:20:17 +0100

    show offline mode banner

--- src/components/Screen.tsx
+++ src/components/Screen.tsx
@@ -1,20 +1,24 @@
 import React, { FC } from "react";
 import { ScrollView, ScrollViewProps } from "react-native";
 import styled from "../styled";
+import OfflineModeBanner from "./OfflineModeBanner";
 
 interface ScreenProps {
   scroll?: boolean;
+  offlineBanner?: boolean;
   style?: Record<string, any>;
   scrollViewProps?: ScrollViewProps;
 }
 
 export const Screen: FC<ScreenProps> = ({
   scroll = false,
+  offlineBanner = true,
   children,
   scrollViewProps,
   ...rest
 }) => (
   <ScreenView {...rest}>
+    {offlineBanner && <OfflineModeBanner />}
     {scroll ? (
       <ScrollView {...scrollViewProps}>{children}</ScrollView>
     ) : (
