9120bee1ee1a07b88989aed13c563539edfda33c
Ismael Haddad <ismael.haddad@prototyp.se>
2021-03-05 09:10:59 +0100

    generate correct geometries

--- src/utils/geoUtils.ts
+++ src/utils/geoUtils.ts
@@ -1,3 +1,8 @@
+import { Geometry } from "../models";
+import { NewFeaturePayload } from "../store/slices/features";
+import convex from "@turf/convex";
+import { point, featureCollection, lineString, Position } from "@turf/helpers";
+
 export function formatGPSLocation(dd: number, ref: string) {
   if (ref == "S" || ref == "W") {
     dd = dd * -1;
@@ -20,4 +25,61 @@ export function getImageLocation(image: any) {
         image.exif &&
         formatGPSLocation(image.exif.GPSLatitude, image.exif.GPSLatitudeRef),
     };
+}
+
+export function getGeometryFromFeatures(
+  features: Array<NewFeaturePayload>
+): Geometry {
+  // If no feature just return Point at 0 0
+  if (features.length === 0)
+    return {
+      type: "Point",
+      coordinates: [0, 0],
+    };
+
+  if (features.length === 1) {
+    return getPoint(features);
+  } else if (features.length === 2) {
+    return getLine(features);
+  } else {
+    return getConvexHullPolygon(features);
+  }
+}
+
+function getPoint(features: Array<NewFeaturePayload>) {
+  return {
+    type: "Point",
+    coordinates: [
+      features[0].imageGPSLatitude as number,
+      features[0].imageGPSLongitude as number,
+    ],
+  };
+}
+
+function getLine(features: Array<NewFeaturePayload>) {
+  const point1: Position = [
+    features[0].imageGPSLatitude as number,
+    features[0].imageGPSLongitude as number,
+  ];
+  const point2: Position = [
+    features[1].imageGPSLatitude as number,
+    features[1].imageGPSLongitude as number,
+  ];
+  const line = lineString([point1, point2]);
+  return line.geometry;
+}
+
+function getConvexHullPolygon(features: Array<NewFeaturePayload>) {
+  const coords: Array<Array<number>> = features.map((feature) => [
+    feature.imageGPSLatitude as number,
+    feature.imageGPSLongitude as number,
+  ]);
+  const points = featureCollection(coords.map((coord) => point(coord)));
+  const hul = convex(points);
+  return (
+    hul?.geometry || {
+      type: "Polygon",
+      coordinates: [],
+    }
+  );
 }
