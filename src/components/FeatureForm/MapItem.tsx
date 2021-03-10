33b3729838ea3ec16595179e8a5b35010f9ee359
simon-lindholm <simon.lindholm@prototyp.se>
2021-03-10 11:53:37 +0100

    style location picker marker

--- src/components/FeatureForm/MapItem.tsx
+++ src/components/FeatureForm/MapItem.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import MapView, { LatLng, Marker } from "react-native-maps";
 import LocationPicker from "./LocationPicker";
+import { theme } from '../../theme';
+import { View, Text } from "react-native";
 
 export default function MapItem({
   location,
@@ -27,7 +29,14 @@ export default function MapItem({
           longitudeDelta: 0.0921,
         }}
       >
-        <Marker pinColor={'turquoise'} coordinate={location} />
+        <View>
+          <Marker pinColor={theme.color.palette.cyan} coordinate={location} />
+          <Marker coordinate={location}>
+            <View style={{ backgroundColor: theme.color.palette.gray, position: 'relative', top: 28}}>
+              <Text style={{fontSize: 14, color: theme.color.palette.cyan, fontFamily: theme.typography.primaryBold, padding: 6}}>Suggested position</Text>
+            </View>
+          </Marker>
+        </View>
       </MapView>
       <LocationPicker
         initLocation={location}
