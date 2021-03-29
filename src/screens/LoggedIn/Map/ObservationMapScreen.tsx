4dae03086434357565841b71aa6634d53e2b0a6b
simon-lindholm <simon.lindholm@prototyp.se>
2021-03-29 12:31:44 +0200

    map final tweaks

--- src/screens/LoggedIn/Map/ObservationMapScreen.tsx
+++ src/screens/LoggedIn/Map/ObservationMapScreen.tsx
@@ -1,36 +1,88 @@
-import * as React from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import styled from "../../../styled";
 import MapView, { Callout, Marker } from "react-native-maps";
 import { Dimensions, View, Image } from "react-native";
 import { useSelector } from "react-redux";
 import { RootState, useThunkDispatch } from "../../../store/store";
-import { Geometry, Observation } from "../../../models";
+import { Geometry, Observation, User } from "../../../models";
 import { theme } from "../../../theme";
 import { NavigationProps } from "../../../navigation/types";
-import { selectObservationDetails } from "../../../store/slices/observations";
-import { getLatLng } from '../../../utils/geoUtils';
+import { fetchObservationCreator, selectObservationDetails } from "../../../store/slices/observations";
+import { getLatLng, isValidMapPoint } from '../../../utils/geoUtils';
 
 export default function ObservationMapScreen({ navigation }:NavigationProps) {
 
   const dispatch = useThunkDispatch();
 
+  const [selectedObservation, setSelectedObservation]:any = useState(null);
+
+  const user = useSelector<RootState, User | undefined>(
+    (state) => state.account.user
+  );
+
   const observationsEntries = useSelector<RootState, Array<Observation>>(
     (state) => state.observations.observationEntries
   );
 
-  const handleOnPress = (index: number) => {
-    const selectedEntry = observationsEntries[index];
+  const observationUsers = useSelector<RootState, Array<User>>(
+    (state) => state.observations.observationUsers
+  )
 
-    dispatch(selectObservationDetails(selectedEntry));
+  const handleOnPinPress = async (index: number) => {
+    
+    if(!observationUsers.find(u => u.id === observationsEntries[index].creatorId) && observationsEntries[index].creatorId !== user?.id) {
+      dispatch(fetchObservationCreator({ creatorId: observationsEntries[index].creatorId })); 
+    }
+    
+    setSelectedObservation(observationsEntries[index]);
+  }
+
+  const handleOnCalloutPress = () => {
+    dispatch(selectObservationDetails(selectedObservation));
     navigation.navigate("observationDetailScreen");
   }
 
-  const isValidGeometry = (geometry: Geometry): boolean => {
-    if(!geometry || !geometry.coordinates) return false;
-    if(geometry.coordinates.length < 2) return false;
-    if(geometry.coordinates[0] === undefined || geometry.coordinates[1] === undefined) return false;
-    return true;
+  const getCreatorName = (observation: Observation) => {
+    
+    const isCreator = (observation.creatorId === user?.id);
+    const fetchedCreator = observationUsers.find(u => u.id === observation.creatorId);
+
+    if(isCreator) return `${user?.givenNames} ${user?.familyName}`
+
+    if(fetchedCreator)
+      return fetchedCreator ? `${fetchedCreator.givenNames} ${fetchedCreator.familyName}` : "unknown";
+    else {
+      return "Observer";
+    }
   }
+
+  const renderObservationDetails = useCallback((observationEntry: Observation) => {
+    
+    const imageUrl = observationEntry.images && observationEntry.images.length>0 ? observationEntry.images[0].url : undefined;
+    const creatorName = getCreatorName(observationEntry);
+
+    return <>
+      { imageUrl && (
+        <CalloutImage source={{uri: imageUrl }} />
+      )}
+      <View style={{flexDirection: 'column'}}>
+        <Title>Observer: { creatorName }</Title>
+        <Text>{"Date: "}
+          {observationEntry.timestamp
+            ? new Date(observationEntry.timestamp)
+                .toUTCString()
+                .slice(5, 17)
+            : ""}
+        </Text>
+        { observationEntry.comments && observationEntry.comments !== undefined && observationEntry.comments !== "" ? (
+          <Text style={{ maxWidth: 100 }}>
+            {observationEntry.comments}
+          </Text>
+        ): <Text>No comment</Text>}
+      </View>
+    </>
+  }, [observationUsers]);
+
 
   return (
     <Screen>
@@ -41,26 +93,17 @@ export default function ObservationMapScreen({ navigation }:NavigationProps) {
         }}
       >
         {observationsEntries.map((observationEntry, index) => {
-          if(isValidGeometry(observationEntry.geometry)) { 
-
-            const imageUrl = observationEntry.images && observationEntry.images.length>0 ? observationEntry.images[0].url : undefined;
-
+          if(isValidMapPoint(observationEntry.geometry)) { 
             return <Marker 
               key={index} 
               coordinate={getLatLng(observationEntry.geometry.coordinates)} 
-              onCalloutPress={() => handleOnPress(index)}
+              onPress={() => handleOnPinPress(index)}
+              onCalloutPress={handleOnCalloutPress}
               pinColor={theme.color.palette.cyan} 
               >
                 <Callout>
                   <CustomCallOutView>
-                    { imageUrl && (
-                      <Image source={{uri: imageUrl }} style={{width: 50, height: 50}}/>
-                    )}
-                    { observationEntry.comments !== undefined && (
-                      <Text style={{ maxWidth: 100, fontSize: 12, fontFamily: theme.typography.primary }}>
-                        {observationEntry.comments}
-                      </Text>
-                    )}
+                    { renderObservationDetails(observationEntry)}
                   </CustomCallOutView>
                 </Callout>
               </Marker>
@@ -77,10 +120,16 @@ const Screen = styled.View`
   justify-content: center;
 `;
 
+
+const CalloutImage = styled.Image`
+  height: 50px;
+  width: 50px;
+  margin-right: ${theme.spacing.small}px;
+`;
+
 const CustomCallOutView = styled.View`
-  flex: 1;
-  align-items: center;
-  justify-content: center;
+  display: flex;
+  flex-direction: row;
 `;
 
 const MarkerPopup = styled.View`
@@ -88,7 +137,12 @@ const MarkerPopup = styled.View`
   background-color: white;
 `;
 
-const Text = styled.Text`
+const Title = styled.Text`
   font-family: ${(props) => props.theme.typography.primaryBold};
-  font-size: ${(props) => props.theme.fontSize.large}px;
+  font-size: ${(props) => props.theme.fontSize.tiny}px;
+`;
+
+const Text = styled.Text`
+  font-family: ${(props) => props.theme.typography.primary};
+  font-size: ${(props) => props.theme.fontSize.tiny}px;
 `;
