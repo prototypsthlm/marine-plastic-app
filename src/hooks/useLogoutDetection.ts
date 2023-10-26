fbd0c3b09a7116c57548ab0bc190bcb8d4b16c66
Simon Kropp <simon.kropp@prototyp.se>
2023-10-26 15:08:02 +0200

    update hook

--- src/hooks/useLogoutDetection.ts
+++ src/hooks/useLogoutDetection.ts
@@ -1,10 +1,17 @@
 import { useSelector } from "react-redux";
 import { selectIsLoggedIn } from "../store/slices/session/selectors";
 import { useEffect, useState } from "react";
-import { RootState } from "../store/store";
-import { initialState } from "../store/slices/session";
+import { RootState, useThunkDispatch } from "../store/store";
+import {
+  initialState as sessionInitialState,
+  userLoggedInOffline,
+} from "../store/slices/session";
+import { setUserInfoOffline } from "../store/slices/account";
 
 export const useLogoutDetection = ({ isOnline }: { isOnline: boolean }) => {
+  const dispatch = useThunkDispatch();
+
+  const user = useSelector((state: RootState) => state.account.user);
   const session = useSelector((state: RootState) => state.session);
   const hasToken = useSelector(selectIsLoggedIn);
 
@@ -16,6 +23,7 @@ export const useLogoutDetection = ({ isOnline }: { isOnline: boolean }) => {
     console.log(" ");
     console.log("isOnline: ", isOnline);
     console.log("hasToken: ", hasToken);
+    console.log("local user:", user);
     console.log("session: ", JSON.stringify(session, null, 4));
     console.log("shouldBeLoggedOut:", shouldBeLoggedOut);
     console.log(" ");
@@ -26,23 +34,39 @@ export const useLogoutDetection = ({ isOnline }: { isOnline: boolean }) => {
   };
 
   useEffect(() => {
+    // Get user from localStorage if offline
+    if (!isOnline && !user) {
+      dispatch(setUserInfoOffline());
+    }
+  }, [isOnline]);
+
+  useEffect(() => {
+    // Log in offline
+    if (!isOnline && user && session === sessionInitialState) {
+      dispatch(userLoggedInOffline({ email: user.email }));
+    }
+  }, [user, isOnline]);
+
+  useEffect(() => {
+    /**
+     * Note:
+     * The session state will be in initialState if we are closing the app and then starting it again when offline (the app won't be able to fetch token)
+     * User data is stored in LocalStorage on login, so that is the only thing we can use to know if the user historically logged in or not
+     */
+
     if (!isOnline) {
-      /**
-       *  If the app is offline with no internet connection we want to prevent logging out the user
-       *  If the session is in an intial state that means that the user probably actively logged out of the app
-       *  Otherwise keep the user logged in until there is an internet connection
-       */
-      if (session === initialState) {
+      if (session === sessionInitialState) {
         setShouldBeLoggedOut(true);
         return;
       }
 
+      setShouldBeLoggedOut(false);
       return;
     }
 
     /** If the app is online just do the normal token check  */
     setShouldBeLoggedOut(!hasToken);
-  }, [session, isOnline]);
+  }, [session, isOnline, user]);
 
   return shouldBeLoggedOut;
 };
