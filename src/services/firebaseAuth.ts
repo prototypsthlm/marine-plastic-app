3030c4b405dba60a2030631c78b66b0c33e23d32
Ismael Haddad <ismael.haddad@prototyp.se>
2023-11-24 11:31:58 +0100

    set up new EAS update, build & submit

--- src/services/firebaseAuth.ts
+++ src/services/firebaseAuth.ts
@@ -1,24 +1,15 @@
 import { initializeApp } from "firebase/app";
 import { initializeAuth, getAuth, getReactNativePersistence } from 'firebase/auth';
 import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
-import {
-  API_KEY,
-  AUTH_DOMAIN,
-  DATABASE_URL,
-  PROJECT_ID,
-  STORAGE_BUCKET,
-  MESSAGE_SENDER_ID,
-  APP_ID,
-} from "@env";
 
 const firebaseConfig = {
-    apiKey: API_KEY,
-    authDomain: AUTH_DOMAIN,
-    databaseURL: DATABASE_URL,
-    projectId: PROJECT_ID,
-    storageBucket: STORAGE_BUCKET,
-    messagingSenderId: MESSAGE_SENDER_ID,
-    appId: APP_ID,
+    apiKey: process.env.EXPO_PUBLIC_API_KEY,
+    authDomain: process.env.EXPO_PUBLIC_AUTH_DOMAIN,
+    databaseURL: process.env.EXPO_PUBLIC_DATABASE_URL,
+    projectId: process.env.EXPO_PUBLIC_PROJECT_ID,
+    storageBucket: process.env.EXPO_PUBLIC_STORAGE_BUCKET,
+    messagingSenderId: process.env.EXPO_PUBLIC_MESSAGE_SENDER_ID,
+    appId: process.env.EXPO_PUBLIC_APP_ID,
   }
 
 const initAuth = () => {
